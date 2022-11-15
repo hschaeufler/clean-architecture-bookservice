6fd8ca52833810a0decf604cc01d9dbfdbbdee74
Holger Schäufler <hlsch@gmx.net>
2022-11-15 04:57:55 +0100

    Update Readme and GetBooksInteractor

--- policy/src/main/java/de/hschaeufler/bookshop/bookservice/policy/getbooks/usecase/GetBooksInteractor.java
+++ policy/src/main/java/de/hschaeufler/bookshop/bookservice/policy/getbooks/usecase/GetBooksInteractor.java
@@ -1,10 +1,8 @@
 package de.hschaeufler.bookshop.bookservice.policy.getbooks.usecase;
 
-import de.hschaeufler.bookshop.bookservice.policy.Book;
 import de.hschaeufler.bookshop.bookservice.policy.getbooks.repository.GetBooksRepository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GetBooksInteractor implements GetBooks {
 
@@ -16,18 +14,6 @@ public class GetBooksInteractor implements GetBooks {
 
     @Override
     public List<GetBooksResponseModel> getBooks() throws NoBooksFoundException {
-        final List<Book> books = this.getBooksRepository.getBooks();
-
-        if (books.isEmpty()) {
-            throw new NoBooksFoundException();
-        }
-
-        return books.stream().map(book -> new GetBooksResponseModel(
-                book.getTitle(),
-                book.getAuthor(),
-                book.getISBN(),
-                books.indexOf(book)
-        )).collect(Collectors.toList());
-
+        throw new RuntimeException("Not Implemented yet!");
     }
 }
