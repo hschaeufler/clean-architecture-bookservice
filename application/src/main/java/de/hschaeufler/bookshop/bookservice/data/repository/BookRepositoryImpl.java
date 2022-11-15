ec8ae87e5c59558ef6fe3f5d31aeb2a15eb095ec
Holger Schäufler <hlsch@gmx.net>
2022-11-15 01:48:31 +0100

    Implement Repository

--- application/src/main/java/de/hschaeufler/bookshop/bookservice/data/repository/BookRepositoryImpl.java
+++ application/src/main/java/de/hschaeufler/bookshop/bookservice/data/repository/BookRepositoryImpl.java
@@ -1,6 +1,8 @@
 package de.hschaeufler.bookshop.bookservice.data.repository;
 
 import de.hschaeufler.bookshop.bookservice.data.datasource.BookDAO;
+import de.hschaeufler.bookshop.bookservice.data.mapper.BookModelMapper;
+import de.hschaeufler.bookshop.bookservice.data.model.BookModel;
 import de.hschaeufler.bookshop.bookservice.domain.Book;
 import de.hschaeufler.bookshop.bookservice.domain.getbooks.repository.GetBooksRepository;
 import de.hschaeufler.bookshop.bookservice.domain.registerbook.repository.RegisterBookRepository;
@@ -10,9 +12,11 @@ import java.util.List;
 public class BookRepositoryImpl implements RegisterBookRepository, GetBooksRepository {
 
     private final BookDAO bookDAO;
+    private final BookModelMapper bookModelMapper;
 
-    public BookRepositoryImpl(BookDAO bookDAO) {
+    public BookRepositoryImpl(BookDAO bookDAO, BookModelMapper bookModelMapper) {
         this.bookDAO = bookDAO;
+        this.bookModelMapper = bookModelMapper;
     }
 
     @Override
@@ -22,11 +26,14 @@ public class BookRepositoryImpl implements RegisterBookRepository, GetBooksRepos
 
     @Override
     public int save(Book book) {
-        return 0;
+        final BookModel bookModel = this.bookModelMapper.toBookModel(book);
+        this.bookDAO.save(bookModel);
+        return (int) this.bookDAO.count();
     }
 
     @Override
     public List<Book> getBooks() {
-        return null;
+        final List<BookModel> bookModels = this.bookDAO.findAll();
+        return this.bookModelMapper.toBookEntities(bookModels);
     }
 }
--- application/src/main/java/de/hschaeufler/bookshop/bookservice/web/controller/BookController.java
+++ application/src/main/java/de/hschaeufler/bookshop/bookservice/web/controller/BookController.java
@@ -1,9 +1,13 @@
 package de.hschaeufler.bookshop.bookservice.web.controller;
+
 import de.hschaeufler.bookshop.bookservice.domain.getbooks.usecase.BookResponseModel;
 import de.hschaeufler.bookshop.bookservice.domain.getbooks.usecase.GetBooks;
+import de.hschaeufler.bookshop.bookservice.domain.getbooks.usecase.NoBooksFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,8 +18,15 @@ public class BookController {
     final GetBooks getBooks;
 
     @GetMapping("/books")
-    List<BookResponseModel> sayHello(){
-        return getBooks.getBooks();
+    List<BookResponseModel> getBooks() {
+        try {
+            return getBooks.getBooks();
+        } catch (NoBooksFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No Books Found!", e);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT, "I'm a Teapot!", e);
+        }
+
     }
 
 }
